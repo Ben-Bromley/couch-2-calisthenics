840bbf781f539782acb60d3e18e5275ee233277e
Ben Bromley <ben.b@reviews.io>
2022-01-24 21:35:02 +0000

    rname file

--- screens/Account.js
+++ screens/Account.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Header from '../components/Header';
-import Nav from '../components/Nav';
-export default function Details({ route }) {
+
+export default function Account({ route }) {
     return (
         <View>
             <Header />
             <View>
-                <Text style={{color: "black", backgroundColor: "orange"}}>{route.params.name}</Text>
+                <Text style={{color: "black", backgroundColor: "orange"}}>{route.name}</Text>
             </View>
-            <Nav />
         </View>
     )
 }
