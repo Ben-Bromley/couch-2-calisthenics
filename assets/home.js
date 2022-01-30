3de6be94b9f9b2909a07b93c3798e51672c63ea0
Ben Bromley <ben.b@reviews.io>
2022-01-30 21:22:44 +0000

    increase home icon width

--- assets/home.js
+++ assets/home.js
@@ -5,8 +5,8 @@ export default function HomeIcon(props) {
     return (
         <Svg
             xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
+            width="35"
+            height="35"
             fill={props.color}
             viewBox="0 0 24 24"
         >
